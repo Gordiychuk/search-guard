a71133a21c11f7852dc1cbacebbef2a76d0aada7
Vladimir Gordiychuk <folyga@gmail.com>
2016-08-01 10:40:30 +0300

    Rename SearchGuardRestFilter to more obvious name

--- src/main/java/com/floragunn/searchguard/filter/AuthenticationRestFilter.java
+++ src/main/java/com/floragunn/searchguard/filter/AuthenticationRestFilter.java
@@ -29,12 +29,12 @@ import com.floragunn.searchguard.auditlog.AuditLog;
 import com.floragunn.searchguard.auth.BackendRegistry;
 import com.floragunn.searchguard.support.HeaderHelper;
 
-public class SearchGuardRestFilter extends RestFilter {
+public class AuthenticationRestFilter extends RestFilter {
 
     private final BackendRegistry registry;
     private final AuditLog auditLog;
 
-    public SearchGuardRestFilter(final BackendRegistry registry, AuditLog auditLog) {
+    public AuthenticationRestFilter(final BackendRegistry registry, AuditLog auditLog) {
         super();
         this.registry = registry;
         this.auditLog = auditLog;
